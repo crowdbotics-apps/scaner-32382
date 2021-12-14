528bdfdc768b5e018f6e4d64aa4b884fef01ed87
Crowdbotics <team@crowdbotics.com>
2021-12-14 10:29:04 +0000

    Initial commit.

---
+++ backend/scaner_32382/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for scaner_32382 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scaner_32382.settings')
+
+application = get_wsgi_application()
